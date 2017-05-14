b7f725f3d3c8a2b881cc86627a844cb3f8b5d1e1
Konstantin Makarchev <kostya27@gmail.com>
2017-05-14 12:28:27 +0300

    add break iterator

---
+++ spec/break_iterator_spec.cr
@@ -0,0 +1,15 @@
+require "./spec_helper"
+
+describe "ICU::BreakIterator" do
+  it do
+    ICU::BreakIterator.new("bla haha", LibICU::UBreakIteratorType::Word).to_a.should eq ["bla", " ", "haha"]
+  end
+
+  it do
+    ICU::BreakIterator.new("หน้าแรก", LibICU::UBreakIteratorType::Word).to_a.should eq ["หน้า", "แรก"]
+  end
+
+  it do
+    ICU::BreakIterator.new("My name is Bla. Muhhahhaa....", LibICU::UBreakIteratorType::Sentence).to_a.should eq ["My name is Bla. ", "Muhhahhaa...."]
+  end
+end
