87766e8cc06dab535aba2dc8ac0719a51d0d11f3
Luc Sarzyniec <devel@olbat.net>
2017-05-18 18:44:43 +0200

    Remove the u_ prefix in all generic purposes functions of LibICU

--- src/icu.cr
+++ src/icu.cr
@@ -4,13 +4,13 @@ require "./icu/*"
 module ICU
   def self.check_error!(ustatus : LibICU::UErrorCode)
     if ustatus > LibICU::UErrorCode::UZeroError
-      raise Error.new(String.new(LibICU.u_error_name(ustatus)))
+      raise Error.new(String.new(LibICU.error_name(ustatus)))
     end
   end
 
   def self.uchars_to_string(ustr : Pointer(LibICU::UChar), len) : String
     str = Slice(UInt8).new(len)
-    LibICU.u_u_chars_to_chars(ustr, str, len)
+    LibICU.u_chars_to_chars(ustr, str, len)
     String.new(str)
   end
 
@@ -20,7 +20,7 @@ module ICU
 
   def self.string_to_uchars(str : String) : Pointer(LibICU::UChar)
     ustr = Slice(LibICU::UChar).new(str.size)
-    LibICU.u_chars_to_u_chars(str, ustr, str.size)
+    LibICU.chars_to_u_chars(str, ustr, str.size)
     ustr.to_unsafe
   end
 end
