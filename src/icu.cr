ca0292f948c7264f835ee589c97f67b26cefe82a
Luc Sarzyniec <devel@olbat.net>
2017-05-19 08:24:21 +0200

    Add a wrapper for UChars->String conversions

--- src/icu.cr
+++ src/icu.cr
@@ -7,20 +7,4 @@ module ICU
       raise Error.new(String.new(LibICU.error_name(ustatus)))
     end
   end
-
-  def self.uchars_to_string(ustr : Pointer(LibICU::UChar), len) : String
-    str = Slice(UInt8).new(len)
-    LibICU.u_chars_to_chars(ustr, str, len)
-    String.new(str)
-  end
-
-  def self.uchars_to_string(ustr : Slice(LibICU::UChar), len) : String
-    uchars_to_string(ustr.to_unsafe, len)
-  end
-
-  def self.string_to_uchars(str : String) : Pointer(LibICU::UChar)
-    ustr = Slice(LibICU::UChar).new(str.size)
-    LibICU.chars_to_u_chars(str, ustr, str.size)
-    ustr.to_unsafe
-  end
 end
