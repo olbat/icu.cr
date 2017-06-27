d312bf5a53e05af6c5af798131e2682578dac2f7
Luc Sarzyniec <devel@olbat.net>
2017-06-27 08:45:31 +0200

    Enable linter tests for the bench/ directory

--- bench/bench_helper.cr
+++ bench/bench_helper.cr
@@ -3,5 +3,5 @@ require "benchmark"
 def load_data(filename)
   data = File.read(File.join(__DIR__, filename))
   data = data.gsub(/\s*#[^\n]*$/m, "") # remove comments
-  data.split('\u001F').map!(&.strip) # split on the INFORMATION SEPARATOR char
+  data.split('\u001F').map!(&.strip)   # split on the INFORMATION SEPARATOR char
 end
