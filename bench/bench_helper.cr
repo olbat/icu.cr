a76e5e012784dcdbf9f5198cb66209aef8179a1d
Luc Sarzyniec <devel@olbat.net>
2017-06-28 17:27:06 +0200

    Move data used for testing in the data/ directory

--- bench/bench_helper.cr
+++ bench/bench_helper.cr
@@ -1,7 +1,9 @@
 require "benchmark"
 
+DATA_DIR = File.expand_path(File.join(__DIR__, "..", "data"))
+
 def load_data(filename)
-  data = File.read(File.join(__DIR__, filename))
+  data = File.read(File.join(DATA_DIR, filename))
   data = data.gsub(/\s*#[^\n]*$/m, "") # remove comments
   data.split('\u001F').map!(&.strip)   # split on the INFORMATION SEPARATOR char
 end
