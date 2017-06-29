134152e5345616e014d3ba126e47f516ae9238e6
Luc Sarzyniec <devel@olbat.net>
2017-06-29 17:07:52 +0200

    Add a ICU::UChar alias and the Char#to_uchar method

--- src/icu/uchars.cr
+++ src/icu/uchars.cr
@@ -1,31 +1,41 @@
 # TODO: ugly, find another way to extend the Slice(UInt16) type
 #       (see https://github.com/crystal-lang/crystal/issues/2529)
-struct ICU::UChars
-  # see http://userguide.icu-project.org/dev/codingguidelines#TOC-Primitive-Type
-  @slice : Slice(UInt16)
+module ICU
+  alias UChar = UInt16
 
-  def initialize(size : Int, *, read_only = false)
-    @slice = typeof(@slice).new(size, read_only: read_only)
+  struct UChars
+    # see http://userguide.icu-project.org/dev/codingguidelines#TOC-Primitive-Type
+    @slice : Slice(ICU::UChar)
+
+    def initialize(size : Int, *, read_only = false)
+      @slice = typeof(@slice).new(size, read_only: read_only)
+    end
+
+    def initialize(size : Int, *, read_only = false, &block : Int32 -> _)
+      @slice = typeof(@slice).new(size, read_only: read_only, &block)
+    end
+
+    def initialize(size : Int, *, value : UChar, read_only = false)
+      @slice = typeof(@slice).new(size, value, read_only: read_only)
+    end
+
+    def initialize(pointer : Pointer(UChar), size : Int, *, read_only = false)
+      @slice = typeof(@slice).new(pointer, size, read_only: read_only)
+    end
+
+    forward_missing_to @slice
+
+    # see http://userguide.icu-project.org/strings#TOC-ICU:-16-bit-Unicode-strings
+    def to_s(size : Int? = nil)
+      size ||= @slice.size
+      String.build { |io| size.times { |i| io << @slice[i].chr } }
+    end
   end
+end
 
-  def initialize(size : Int, *, read_only = false, &block : Int32 -> _)
-    @slice = typeof(@slice).new(size, read_only: read_only, &block)
-  end
-
-  def initialize(size : Int, *, value : UInt16, read_only = false)
-    @slice = typeof(@slice).new(size, value, read_only: read_only)
-  end
-
-  def initialize(pointer : Pointer(UInt16), size : Int, *, read_only = false)
-    @slice = typeof(@slice).new(pointer, size, read_only: read_only)
-  end
-
-  forward_missing_to @slice
-
-  # see http://userguide.icu-project.org/strings#TOC-ICU:-16-bit-Unicode-strings
-  def to_s(size : Int? = nil)
-    size ||= @slice.size
-    String.build { |io| size.times { |i| io << @slice[i].chr } }
+struct Char
+  def to_uchar
+    ord.to_u16
   end
 end
 
@@ -36,7 +46,7 @@ class String
     size ||= size()
     ICU::UChars.new(size).tap do |uchars|
       each_char do |c|
-        uchars[i] = c.ord.to_u16
+        uchars[i] = c.to_uchar
         i += 1
       end
     end
