eee6c5491bbc9a6e9d84ad3ae045697456a6e84e
Luc Sarzyniec <devel@olbat.net>
2017-07-06 17:23:20 +0200

    Add locale related constants

---
+++ src/icu/locale.cr
@@ -0,0 +1,14 @@
+class ICU::Locale
+  LANG_MAX_SIZE                =  12
+  COUNTRY_MAX_SIZE             =   4
+  FULLNAME_MAX_SIZE            = 157
+  SCRIPT_MAX_SIZE              =   6
+  KEYWORDS_MAX_SIZE            =  96
+  KEYWORDS_AND_VALUES_MAX_SIZE = 100
+
+  class Keyword
+    SEPARATOR      = '@'
+    ASSIGN         = '='
+    ITEM_SEPARATOR = ';'
+  end
+end
