87dd82f117df81beb374f501037b842982fbc810
Luc Sarzyniec <devel@olbat.net>
2017-07-12 08:49:14 +0200

    Add new methods to Collator
    Add the KEYWORDS constant and the functional_equivalence, attributes, strength
    and reorder_codes accessor methods

--- spec/collator_spec.cr
+++ spec/collator_spec.cr
@@ -1,6 +1,19 @@
 require "./spec_helper"
 
 describe "ICU::Collator" do
+  describe "LOCALES" do
+    it "lists available locales" do
+      ICU::Collator::LOCALES.includes?("en").should be_true
+    end
+  end
+
+  describe "KEYWORDS" do
+    it "lists available keywords" do
+      ICU::Collator::KEYWORDS["collation"]?.should_not be_nil
+      ICU::Collator::KEYWORDS["collation"].includes?("standard").should be_true
+    end
+  end
+
   describe "initialize" do
     it "creates a new Collator without specifying a locale" do
       ICU::Collator.new.should_not be_nil
@@ -18,6 +31,12 @@ describe "ICU::Collator" do
       expect_raises do
         ICU::Collator.new("not_a_locale")
       end
+    end
+  end
+
+  describe "functional_equivalent" do
+    it "returns a functional equivalent to a given locale" do
+      ICU::Collator.functional_equivalent("en").should eq("root")
     end
   end
 
@@ -66,6 +85,68 @@ describe "ICU::Collator" do
       col = ICU::Collator.new("en")
       col.equals?("a", "b").should be_false
       col.equals?("abc", "abd").should be_false
+    end
+  end
+
+  describe "[]" do
+    it "returns the value of the specified attribute" do
+      nm = ICU::Collator::ON
+      col = ICU::Collator.new("&c < b < a".to_uchars, nm)
+      col[ICU::Collator::Attribute::NormalizationMode].should eq(nm)
+    end
+  end
+
+  describe "[]=" do
+    it "set a value to the specified attribute" do
+      col = ICU::Collator.new("&c < b < a".to_uchars, ICU::Collator::ON)
+      nm = ICU::Collator::OFF
+      col[ICU::Collator::Attribute::NormalizationMode] = nm
+      col[ICU::Collator::Attribute::NormalizationMode].should eq(nm)
+    end
+  end
+
+  describe "strength" do
+    it "returns the strength" do
+      str = ICU::Collator::Strength::Secondary
+      col = ICU::Collator.new("&c < b < a".to_uchars, strength: str)
+      col.strength.should eq(str)
+      col.strength.should eq(col[ICU::Collator::Attribute::Strength])
+    end
+  end
+
+  describe "strength=" do
+    it "set the strength" do
+      col = ICU::Collator.new("&c < b < a".to_uchars, strength: ICU::Collator::Strength::Tertiary)
+      str = ICU::Collator::Strength::Secondary
+      col.strength = str
+      col.strength.should eq(str)
+    end
+  end
+
+  describe "reorder_codes" do
+    it "returns the reorder codes" do
+      col = ICU::Collator.new
+      rc = [ICU::Collator::ReorderCode::ReorderCodeSymbol]
+      col.reorder_codes = rc
+      col.reorder_codes.should eq(rc)
+    end
+
+    it "returns an empty array if no reorder codes were set" do
+      col = ICU::Collator.new
+      col.reorder_codes.should_not be_nil
+      col.reorder_codes.empty?.should be_true
+    end
+  end
+
+  describe "reorder_codes=" do
+    it "set the reorder codes" do
+      col = ICU::Collator.new
+      rc = [
+        ICU::Collator::ReorderCode::ReorderCodePunctuation,
+        ICU::Collator::ReorderCode::ReorderCodeSymbol,
+      ]
+      col.reorder_codes = rc
+      col.reorder_codes.should eq(rc)
     end
   end
 end
