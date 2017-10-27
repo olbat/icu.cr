15d8e1f28378eef14c9ded34a2337546b8f24683
Luc Sarzyniec <devel@olbat.net>
2017-10-27 17:33:33 +0200

    Add examples in the IDNA's doc

--- src/icu/idna.cr
+++ src/icu/idna.cr
@@ -1,8 +1,18 @@
-# International Domain Names in Applications
+# __International Domain Names in Applications__
 #
-# See also:
+# Implements [UTS #46](http://unicode.org/reports/tr46/) and [IDNA2008](https://tools.ietf.org/html/rfc5890).
+#
+# __Usage__
+# ```
+# dn = ICU::IDNA.to_ascii("افغانستا.icom.museum")                             # => "xn--mgbaal8b0b9b2b.icom.museum"
+# ICU::IDNA.to_unicode(dn)                                                    # => "افغانستا.icom.museum"
+# ICU::IDNA.compare("افغانستا.icom.museum", "xn--mgbaal8b0b9b2b.icom.museum") # => 0
+# ```
+#
+# __See also__
 # - [reference implementation](http://icu-project.org/apiref/icu4c/uidna_8h.html)
 # - [user guide](http://userguide.icu-project.org/strings/stringprep#TOC-IDNA-API-in-ICU)
+# - [unit tests](https://github.com/olbat/icu.cr/blob/master/spec/idna_spec.cr)
 class ICU::IDNA
   # FIXME: should be present in LibICU (not parsed by Libgen)
   @[Flags]
@@ -16,6 +26,11 @@ class ICU::IDNA
     CheckContexto            = 0x40
   end
 
+  # Converts a Unicode IDN to it's ASCII representation
+  #
+  # ```
+  # ICU::IDNA.to_ascii("افغانستا.icom.museum") # => "xn--mgbaal8b0b9b2b.icom.museum"
+  # ```
   def self.to_ascii(unicode_idn : String, options : UInt32 = Option::Default.to_u32) : String
     src = unicode_idn.to_uchars
     dst = ICU::UChars.new(src.size * 2)
@@ -28,6 +43,11 @@ class ICU::IDNA
     dst.to_s(size)
   end
 
+  # Converts an ASCII IDN to it's Unicode representation
+  #
+  # ```
+  # ICU::IDNA.to_unicode("xn--mgbaal8b0b9b2b.icom.museum") # => "افغانستا.icom.museum"
+  # ```
   def self.to_unicode(ascii_idn : String, options : UInt32 = Option::Default.to_u32) : String
     src = ascii_idn.to_uchars
     dst = ICU::UChars.new(src.size)
@@ -40,6 +60,13 @@ class ICU::IDNA
     dst.to_s(size)
   end
 
+  # Compares two IDN strings (that can be in unicode or ascii representation)
+  #
+  # ```
+  # ICU::IDNA.compare("افغانستا.icom.museum", "xn--mgbaal8b0b9b2b.icom.museum") # => 0
+  # ```
+  #
+  # (see `String#compare`)
   def self.compare(idn1 : String, idn2 : String, options : UInt32 = Option::Default.to_u32) : Int
     idn1 = idn1.to_uchars
     idn2 = idn2.to_uchars
