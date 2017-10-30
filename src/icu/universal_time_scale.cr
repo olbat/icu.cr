7e7e967d613ecbf5e8c0c7af901152324d976faa
Luc Sarzyniec <devel@olbat.net>
2017-10-30 18:03:56 +0100

    Add examples in the UniversalTimeScale's doc

--- src/icu/universal_time_scale.cr
+++ src/icu/universal_time_scale.cr
@@ -1,8 +1,16 @@
-# Universal Time Scale
+# __Universal Time Scale__
+# Convert dates between binary datetime conventions
 #
-# See also:
+# __Usage__
+# ```
+# ts = ICU::UniversalTimeScale::DateTimeScale::UnixTime
+# ICU::UniversalTimeScale.from(0.to_i64, ts) # => (ICU::UniversalTimeScale::TIME_SCALES[ts][:epoch_offset] * (10**7))
+# ```
+#
+# __See also__
 # - [reference implementation](http://icu-project.org/apiref/icu4c/utmscale_8h.html)
 # - [user guide](http://userguide.icu-project.org/datetime/universaltimescale)
+# - [unit tests](https://github.com/olbat/icu.cr/blob/master/spec/universal_time_scale_spec.cr)
 class ICU::UniversalTimeScale
   alias UniversalTime = Int64
   alias DateTimeScale = LibICU::UDateTimeScale
@@ -18,6 +26,7 @@ class ICU::UniversalTimeScale
 
   alias TimeScaleValues = NamedTuple(precision: Int64, epoch_offset: Int64, from_min: Int64, from_max: Int64, to_min: Int64, to_max: Int64)
 
+  # A list of the supported time scales
   TIME_SCALES = begin
     tss = Hash(DateTimeScale, TimeScaleValues).new
     LibICU::UDateTimeScale.each do |time_scale, _|
@@ -38,6 +47,7 @@ class ICU::UniversalTimeScale
     tss
   end
 
+  # Converts a time using the _time_scale_ date convention to a Universal time
   def self.from(time : Int64, time_scale : DateTimeScale) : UniversalTime
     ustatus = LibICU::UErrorCode::UZeroError
     ret = LibICU.utmscale_from_int64(time, time_scale, pointerof(ustatus))
@@ -45,6 +55,7 @@ class ICU::UniversalTimeScale
     ret
   end
 
+  # Converts a UniversalTime to a time using the _time_scale_ date convention
   def self.to(universal_time : UniversalTime, time_scale : DateTimeScale) : Int64
     ustatus = LibICU::UErrorCode::UZeroError
     ret = LibICU.utmscale_to_int64(universal_time, time_scale, pointerof(ustatus))
