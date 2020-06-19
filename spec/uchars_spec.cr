fdbf075316a764560332c2460e9d81320b6fcdb0
Luc Sarzyniec <devel@olbat.net>
2020-06-19 18:26:54 +0200

    Fix: replace String#at calls deprecated in Crystal 0.33 by String#char_at

--- spec/uchars_spec.cr
+++ spec/uchars_spec.cr
@@ -4,7 +4,7 @@ describe "ICU::UChars" do
   describe "initialize" do
     it "creates a valid UChars object from a String" do
       str = "Σὲ γνωρίζω ἀπὸ τὴν κόψη"
-      src = ICU::UChars.new(str.size) { |i| str.at(i).ord.to_u16 }
+      src = ICU::UChars.new(str.size) { |i| str.char_at(i).ord.to_u16 }
       dest = ICU::UChars.new(str.size)
 
       # check that the UChars object is valid using a function of the lib
